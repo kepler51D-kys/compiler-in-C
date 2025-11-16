66ba235235e6abfa346727db3cf78fecc77847cc
PilkeySEK <PilkeySEK@gmx.de>
2025-11-16 11:47:31 +0100

    sorgy i forgot to format the tokenizer

--- src/lexer/tokenizer.c
+++ src/lexer/tokenizer.c
@@ -2,11 +2,11 @@
 #define TOKENIZER_C
 
 typedef enum Token {
-    token_eof,
-    token_binary_operation,
-    token_rparen,
-    token_lparen,
-    token_identifier,
+  token_eof,
+  token_binary_operation,
+  token_rparen,
+  token_lparen,
+  token_identifier,
 } Token;
 
 #endif // TOKENIZER_C
