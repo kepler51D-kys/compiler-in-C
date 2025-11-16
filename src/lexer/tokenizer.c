ca21c770652178680283d761f869fd784540e271
PilkeySEK <PilkeySEK@gmx.de>
2025-11-16 11:36:30 +0100

    silly linked list

---
+++ src/lexer/tokenizer.c
@@ -0,0 +1,12 @@
+#ifndef TOKENIZER_C
+#define TOKENIZER_C
+
+typedef enum Token {
+    token_eof,
+    token_binary_operation,
+    token_rparen,
+    token_lparen,
+    token_identifier,
+} Token;
+
+#endif // TOKENIZER_C
